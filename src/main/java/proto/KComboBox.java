9d5d00c393f3da169f1855442b6a4b6b78b7cd39
wakadrammeh <wakadrammeh@gmail.com>
2021-04-26 16:23:23 +0000

    Update pom.xml

--- src/main/java/proto/KComboBox.java
+++ src/main/java/proto/KComboBox.java
@@ -3,58 +3,52 @@ package proto;
 import core.utils.MComponent;
 
 import javax.swing.*;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class KComboBox<E> extends JComboBox<E> implements Preference {
-    /**
-     * Refers to an element in the model,
-     * such that {@link #getSelectionText()}
-     * should not return its string value.
-     * Precisely, this is usually a descriptively default class property
-     * used by this instance, and selection of such property signifies
-     * no selection, hence the {@link #value} should be returned
-     * as the property in-place.
-     * For e.g. some fields of {@link core.module.Course}: day, time, etc.
-     * This allows such fields to be presented indirectly.
-     */
-    private E mask;
-    private E value;
+    private HashMap<String, Object> masks;
 
 
     public KComboBox(E[] items){
         super(items);
+        masks = new HashMap<>();
         setPreferences();
     }
 
-    @Override
-    public JToolTip createToolTip() {
-        return MComponent.preferredTip();
+    public KComboBox(E[] items, int i){
+        this(items);
+        if (i == -1) {
+            setSelectedIndex(getItemCount() - 1);
+        } else {
+            setSelectedIndex(i);
+        }
     }
 
-    /**
-     * Sets the exception with its alternative for this model.
-     * This instance will now set selection to this exception element.
-     */
-    public void setMask(E except, E alt) {
-        this.mask = except;
-        this.value = alt;
-        this.setSelectedItem(mask);
+    public void addMask(E mask, Object value) {
+        masks.put(String.valueOf(mask), value);
     }
 
     /**
      * Returns a string representation of whatever element
      * is currently selected from this comboBox.
-     * If this instance has specifies a mask,
+     * If this instance has specifies a mask for the item,
      * and the selected item is such, then the alternative
-     * (actual) value ({@link #value}) will be returned instead.
+     * (actual) value will be returned instead.
      */
     public String getSelectionText() {
         final Object selection = getSelectedItem();
-        if (Objects.equals(selection, mask)) {
-            return String.valueOf(value);
+        final String selectionText = String.valueOf(selection);
+        if (masks.containsKey(selectionText)) {
+            return String.valueOf(masks.get(selectionText));
         } else {
-            return String.valueOf(selection);
+            return selectionText;
         }
+    }
+
+    @Override
+    public JToolTip createToolTip() {
+        return MComponent.preferredTip();
     }
 
     @Override
